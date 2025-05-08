92faaa23642eaa23daece7babd96ac393a218440
Natanael A. de Morais <natanaelallves@gmail.com>
2025-05-08 19:58:56 -0300

    Add dark mode warning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue'
 
+const darkModeOff: Ref<boolean> = ref(false)
 const light: Ref<boolean> = ref(true)
 const msg: Ref<string> = ref('')
 
@@ -9,18 +10,18 @@ function toggleLight() {
 }
 
 function tentarSenha() {
-  msg.value = '( ainda não terminei essa parte :[ )'
+  msg.value = '( se está vendo essa mensagem, pabéns, vc não acertou a senha! mas calma, é pq eu ainda não terminei. haverá mais em breve, então, até logo! )'
 
   setTimeout(() => {
     msg.value = ''
-  }, 5000);
+  }, 12000);
 }
 </script>
 
 <template class="">
   <div class="h-screen flex flex-col" :class="{ 'bg-white': light, 'bg-black': !light }">
-    <div>WIP Enigmas... (fala rapaziada!)</div>
-    <div class="text-sm text-gray-500 mb-5 w-fit">
+    <div class="px-4">WIP Enigmas... (fala rapaziada!)</div>
+    <div class="text-sm text-gray-500 mb-5 w-fit px-4">
       <i>
         <strong>Aviso</strong>: não utilize o "Inspecionar" a página para resolver, pois isso é trapaça.
         <strong>explore</strong>
@@ -32,7 +33,7 @@ function tentarSenha() {
     <div class="text-white w-full max-w-md mx-auto grow flex justify-center items-center border border-white mb-5">
       <form class="select-none" @submit.prevent="tentarSenha">
         <p class="text-black mb-5 text-2xl">Informe a senha:</p>
-        <div class="flex gap-3 mb-5">
+        <div class="flex gap-3 mb-5 w-fit mx-auto">
           <input
             type="number"
             class="bg-white text-black text-4xl px-2 w-12 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
@@ -62,18 +63,28 @@ function tentarSenha() {
             max="9"
           >
         </div>
-        <div class="text-end mb-5">
+        <div class="text-end mb-5 mx-auto w-fit">
           <button class="font-bold border border-white px-4 py-2 cursor-pointer" type="submit">Enviar</button>
         </div>
         <div>
-          <p class="text-wrap text-center" v-text="msg"></p>
+          <p class="text-wrap text-center px-4" v-text="msg"></p>
         </div>
       </form>
     </div>
 
-    <div class="bg-white text-center text-sm py-3">
+    <div class="text-center text-sm py-3" :class="{ 'text-white': !light, 'text-black': light }">
         2025&copy;, Feito por <a href="https://github.com/eunael" class="link text-blue-500"
             target="_blank">eunael</a>.
     </div>
+
+    <div
+        v-if="!darkModeOff"
+        class="bg-black text-gray-50 text-nowrap absolute w-full h-screen flex justify-center items-center"
+      >
+        <div class="text-center font-bold px-4">
+          <p class="text-xl mb-5 w-full max-w-md text-wrap">✋ Antes, por favor desative qualquer extenção de dark mode e depois clique em "OK"</p>
+          <button class="px-4 py-2 bg-transparent border border-white cursor-pointer" @click="darkModeOff = true">OK</button>
+        </div>
+      </div>
   </div>
 </template>
